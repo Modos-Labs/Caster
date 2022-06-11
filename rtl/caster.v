1363b3a409d5b2d2bd2d25f7fb8228410e613de9
Wenting Zhang <zephray@outlook.com>
2022-06-11 22:42:22 +0000

    rtl: fast 4 level grayscale mode wip

--- rtl/caster.v
+++ rtl/caster.v
@@ -175,8 +175,19 @@ module caster(
     localparam MODE_FAST_GREY = 2'b10;
     localparam MODE_RESERVED = 2'b11;
 
-    localparam FASTM_B2W_FRAMES = 6'd9;
-    localparam FASTM_W2B_FRAMES = 6'd9;
+    //localparam FASTM_B2W_FRAMES = 6'd9;
+    //localparam FASTM_W2B_FRAMES = 6'd9;
+    localparam FASTM_B2W_FRAMES = 6'd10;
+    localparam FASTM_W2B_FRAMES = 6'd10;
+
+    //
+    localparam FASTG_HOLDOFF_FRAMES = 6'd10;
+    localparam FASTG_B2G_FRAMES = 6'd1;
+    localparam FASTG_W2G_FRAMES = 6'd1;
+    localparam FASTG_LG2B_FRAMES = 6'd8;
+    localparam FASTG_DG2B_FRAMES = 6'd5;
+    localparam FASTG_LG2W_FRAMES = 6'd5;
+    localparam FASTG_DG2W_FRAMES = 6'd8;
 
     // In normal LUT mode:
     // Bit 13: LUT ID
@@ -195,6 +206,18 @@ module caster(
     // Bit 3-1: Must be 0
     // Bit 0: Previous frame pixel value (0 black 1 white)
 
+    // In fast grey 4-level mode:
+    // Bit 13-12: Reserved
+    // Bit 11-10: Stage
+    // Bit 9-4: Frame counter
+    // Bit 3-2: Must be 0
+    // Bit 1-0: Previous frame pixel value
+    // It matches, continue the current operation:
+    localparam STAGE_DONE = 2'd0; // Screen already settled. No operation
+    localparam STAGE_MONO = 2'd1; // Driving to mono (same as fast mono mode)
+    localparam STAGE_HOLD = 2'd2; // Hold off (wait before start driving greyscale)
+    localparam STAGE_GREY = 2'd3; // Driving to greyscale (non-cancellable)
+
     wire [7:0] pixel_comb;
     wire [63:0] bo_pixel_comb;
     genvar i;
@@ -207,11 +230,15 @@ module caster(
             // Pixel processing
             wire [1:0] pixel_mode = proc_bi[15:14];
             wire pixel_lutid = proc_bi[13];
+            wire [1:0] pixel_stage = proc_bi[11:10];
             wire [5:0] pixel_framecnt = proc_bi[9:4];
             wire [3:0] pixel_prev = proc_bi[3:0];
             wire [5:0] pixel_framecnt_dec = pixel_framecnt - 1;
+            // Specific to fast mono mode
             wire [5:0] pixel_framecnt_2w = FASTM_B2W_FRAMES - pixel_framecnt + 2;
             wire [5:0] pixel_framecnt_2b = FASTM_W2B_FRAMES - pixel_framecnt + 2;
+            wire [5:0] pixel_framecnt_back = FASTG_B2G_FRAMES - pixel_framecnt + 1;
+            wire [5:0] pixel_framecnt_oppo = FASTM_B2W_FRAMES - FASTG_B2G_FRAMES + pixel_framecnt;
 
             assign proc_output =
                 (op_state == OP_INIT) ? (
@@ -255,6 +282,24 @@ module caster(
                                 proc_vin[3] ? 2'b10 : 2'b01
                             )
                         )
+                    ) : (pixel_mode == MODE_FAST_GREY) ? (
+                        // Fast grey mode
+                        ((pixel_stage == STAGE_DONE) || (pixel_stage == STAGE_HOLD)) ? (
+                            // Currently not updating
+                            (proc_vin[3:2] == pixel_prev[1:0]) ? (
+                                // Pixel state did not change, no op
+                                2'b00
+                            ) : (
+                                // Pixel state changed, update
+                                proc_vin[3] ? 2'b10 : 2'b01
+                            )
+                        ) : (pixel_stage == STAGE_MONO) ? (
+                            // Fast mono stage, always drive towards input
+                            proc_vin[3] ? 2'b10 : 2'b01
+                        ) : (
+                            // Grey mode, non cancellable, drive towards old value
+                            pixel_prev[1] ? 2'b01 : 2'b10
+                        )
                     ) : 2'b00 // Unknown mode
                 ) : (
                     2'b00 // Unknwon state
@@ -262,7 +307,8 @@ module caster(
             assign proc_bo =
                 (op_state == OP_INIT) ? (
                     {MODE_FAST_MONO, 4'b0, 6'd0, 3'b0, 1'b1}
-                ) : ((op_state == OP_NORMAL) ? (
+                    //{MODE_FAST_GREY, 4'b0, 6'd0, 2'b0, 2'b11}
+                ) : (op_state == OP_NORMAL) ? (
                     // Normal state
                     (pixel_mode == MODE_NORMAL_LUT) ? (
                         // Normal LUT mode
@@ -295,20 +341,96 @@ module caster(
                                 ) : {proc_bi[15:10], FASTM_W2B_FRAMES, 4'd0}
                             )
                         )
+                    ) : (pixel_mode == MODE_FAST_GREY) ? (
+                        // Fast grey mode
+                        (proc_vin[3:2] == pixel_prev[1:0]) ? (
+                            // Pixel didn't change, continue
+                            (pixel_stage == STAGE_DONE) ? (
+                                proc_bi // Stay
+                            ) : (pixel_stage == STAGE_MONO) ? (
+                                (pixel_framecnt == 0) ? (
+                                    {proc_bi[15:12], STAGE_HOLD, FASTG_HOLDOFF_FRAMES, proc_bi[3:0]}
+                                ) : (
+                                    {proc_bi[15:10], pixel_framecnt_dec, proc_bi[3:0]}
+                                )
+                            ) : (pixel_stage == STAGE_HOLD) ? (
+                                (pixel_framecnt == 0) ? (
+                                    (pixel_prev[1:0] == 2'b10) ? (
+                                        {proc_bi[15:12], STAGE_GREY, FASTG_W2G_FRAMES, proc_bi[3:0]}
+                                    ) : (pixel_prev[1:0] == 2'b01) ? (
+                                        {proc_bi[15:12], STAGE_GREY, FASTG_B2G_FRAMES, proc_bi[3:0]}
+                                    ) : (
+                                        {proc_bi[15:12], STAGE_DONE, 6'd0, 2'b00, proc_vin[3:2]}
+                                    )
+                                ) : (
+                                    {proc_bi[15:10], pixel_framecnt_dec, proc_bi[3:0]}
+                                )
+                            ) : (
+                                // Grey
+                                (pixel_framecnt == 0) ? (
+                                    {proc_bi[15:12], STAGE_DONE, 6'd0, proc_bi[3:0]}
+                                ) : (
+                                    {proc_bi[15:10], pixel_framecnt_dec, proc_bi[3:0]}
+                                )
+                            )
+                        ) : (
+                            ((pixel_stage == STAGE_DONE) || (pixel_stage == STAGE_HOLD)) ? (
+                                proc_vin[3] ? (
+                                    (pixel_prev[1:0] == 2'b10) ? (
+                                        {proc_bi[15:12], STAGE_MONO, FASTG_LG2W_FRAMES, 2'b00, proc_vin[3:2]}
+                                    ) : (pixel_prev[1:0] == 2'b01) ? (
+                                        {proc_bi[15:12], STAGE_MONO, FASTG_DG2W_FRAMES, 2'b00, proc_vin[3:2]}
+                                    ) : (
+                                        {proc_bi[15:12], STAGE_MONO, FASTM_B2W_FRAMES, 2'b00, proc_vin[3:2]}
+                                    )
+                                ) : (
+                                    (pixel_prev[1:0] == 2'b10) ? (
+                                        {proc_bi[15:12], STAGE_MONO, FASTG_LG2B_FRAMES, 2'b00, proc_vin[3:2]}
+                                    ) : (pixel_prev[1:0] == 2'b01) ? (
+                                        {proc_bi[15:12], STAGE_MONO, FASTG_DG2B_FRAMES, 2'b00, proc_vin[3:2]}
+                                    ) : (
+                                        {proc_bi[15:12], STAGE_MONO, FASTM_W2B_FRAMES, 2'b00, proc_vin[3:2]}
+                                    )
+                                )
+                            ) : (pixel_stage == STAGE_MONO) ? (
+                                // Pixel state changed, update
+                                proc_vin[3] ? (
+                                    {proc_bi[15:12], STAGE_MONO, pixel_framecnt_2w, 2'b00, proc_vin[3:2]}
+                                ) : (
+                                    {proc_bi[15:12], STAGE_MONO, pixel_framecnt_2b, 2'b00, proc_vin[3:2]}
+                                )
+                            ) : (
+                                // Stage grey
+                                /*(proc_vin[3] == pixel_prev[1]) ? (
+                                    // Back to previous color
+                                    {proc_bi[15:12], STAGE_MONO, pixel_framecnt_back, 2'b00, proc_vin[3:2]}
+                                ) : (
+                                    // To another side
+                                    {proc_bi[15:12], STAGE_MONO, pixel_framecnt_oppo, 2'b00, proc_vin[3:2]}
+                                )*/
+                                // Non cancellable
+                                (pixel_framecnt == 0) ? (
+                                    {proc_bi[15:12], STAGE_DONE, 6'd0, proc_bi[3:0]}
+                                ) : (
+                                    {proc_bi[15:10], pixel_framecnt_dec, proc_bi[3:0]}
+                                )
+                            )
+                        )
                     ) : 16'h00 // Unknown mode
                 ) : (
                     16'h00 // Unknwon state
-                ));
+                );
             // Output
             assign pixel_comb[i*2+1 : i*2] = proc_output;
             assign bo_pixel_comb[i*16+15 : i*16] = proc_bo;
         end
     endgenerate
+
     reg [7:0] current_pixel;
     reg output_mask;
     always @(posedge clk) begin
         output_mask <= proc_in_act;
-        current_pixel <= (proc_in_act) ? pixel_comb : 8'h00;
+        current_pixel <= (output_mask) ? pixel_comb : 8'h00;
         bo_pixel <= bo_pixel_comb;
     end
 
@@ -341,7 +463,7 @@ module caster(
 
     assign bi_ready = proc_in_act;
     assign bo_valid = scan_in_act;
-    
+
     assign b_trigger = (scan_state == SCAN_WAITING);
 
 endmodule
