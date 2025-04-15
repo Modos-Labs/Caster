4b00e6270894ecd0dc42a7a42e83df5ab46d8faf
unknown <Wenting Zhang>
2025-04-15 17:50:36 +0800

    Color mixer support for RGBW screens

--- rtl/spartan6/vin_colormixer.v
+++ rtl/spartan6/vin_colormixer.v
@@ -22,6 +22,8 @@ module vin_colormixer(
     output reg          out_valid
 );
 
+    parameter COLORMODE = "MONO";
+
     wire [5:0] r_even = in_color[47:42];
     wire [5:0] g_even = in_color[39:34];
     wire [5:0] b_even = in_color[31:26];
@@ -33,49 +35,88 @@ module vin_colormixer(
     wire [7:0] y_even;
 
     // Processing for mono
-    rgb2y rgb2y_odd (.r(r_odd), .g(g_odd), .b(b_odd), .y(y_odd));
-    rgb2y rgb2y_even (.r(r_even), .g(g_even), .b(b_even), .y(y_even));
-//    `else
-//        // Processing for DES
-//        reg [1:0] c_cnt_x;
-//        reg [1:0] c_cnt_y;
-//        reg hs_last;
-//        reg first_line;
-//        always @(posedge clk) begin
-//            hs_last <= in_hsync;
-//            if (!hs_last && in_hsync) begin
-//                if (in_vsync) begin
-//                    c_cnt_y <= 2'd1;
-//                    c_cnt_x <= 2'd0;
-//                    first_line <= 1'b1;
-//                end
-//                else if (!first_line) begin
-//                    c_cnt_x <= c_cnt_y;
-//                    if (c_cnt_y == 2'd2) begin
-//                        c_cnt_y <= 2'd0;
-//                    end
-//                    else begin
-//                        c_cnt_y <= c_cnt_y + 1;
-//                    end
-//                end
-//            end
-//            else if (in_valid) begin
-//                first_line <= 1'b0;
-//                if (c_cnt_x == 2'd2) begin
-//                    c_cnt_x <= 2'd0;
-//                end
-//                else begin
-//                    c_cnt_x <= c_cnt_x + 1;
-//                end
-//            end
-//        end
+    generate
+    if (COLORMODE == "MONO") begin: gen_mono_mixer
+        rgb2y rgb2y_odd (.r(r_odd), .g(g_odd), .b(b_odd), .y(y_odd));
+        rgb2y rgb2y_even (.r(r_even), .g(g_even), .b(b_even), .y(y_even));
+    end
+    else if (COLORMODE == "DES") begin: gen_color_mixer
+        // Processing for DES
+        reg [1:0] c_cnt_x;
+        reg [1:0] c_cnt_y;
+        reg hs_last;
+        reg first_line;
+        always @(posedge clk) begin
+            hs_last <= in_hsync;
+            if (!hs_last && in_hsync) begin
+                if (in_vsync) begin
+                    c_cnt_y <= 2'd1;
+                    c_cnt_x <= 2'd0;
+                    first_line <= 1'b1;
+                end
+                else if (!first_line) begin
+                    c_cnt_x <= c_cnt_y;
+                    if (c_cnt_y == 2'd2) begin
+                        c_cnt_y <= 2'd0;
+                    end
+                    else begin
+                        c_cnt_y <= c_cnt_y + 1;
+                    end
+                end
+            end
+            else if (in_valid) begin
+                first_line <= 1'b0;
+                if (c_cnt_x == 2'd2) begin
+                    c_cnt_x <= 2'd0;
+                end
+                else begin
+                    c_cnt_x <= c_cnt_x + 1;
+                end
+            end
+        end
+        // DES
 //        assign y_odd[7:2] = (c_cnt_x == 2'd0) ? (b_odd) :
 //                (c_cnt_x == 2'd1) ? (r_odd) : (g_odd);
 //        assign y_even[7:2] = (c_cnt_x == 2'd0) ? (r_even) :
 //                (c_cnt_x == 2'd1) ? (g_even) : (b_even);
-//        assign y_odd[1:0] = y_odd[7:6];
-//        assign y_even[1:0] = y_even[7:6];
-//    `endif
+        // Kaleido 3
+        assign y_odd[7:2] = (c_cnt_x == 2'd0) ? (r_odd) :
+                (c_cnt_x == 2'd1) ? (b_odd) : (g_odd);
+        assign y_even[7:2] = (c_cnt_x == 2'd0) ? (b_even) :
+                (c_cnt_x == 2'd1) ? (g_even) : (r_even);
+        assign y_odd[1:0] = y_odd[7:6];
+        assign y_even[1:0] = y_even[7:6];    
+    end
+    else if (COLORMODE == "RGBW") begin: gen_rgbw_color_mixer
+        reg c_cnt_y;
+        reg hs_last;
+        reg first_line;
+        always @(posedge clk) begin
+            hs_last <= in_hsync;
+            if (!hs_last && in_hsync) begin
+                if (in_vsync) begin
+                    c_cnt_y <= 1'd0;
+                    first_line <= 1'b1;
+                end
+                else if (!first_line) begin
+                    c_cnt_y <= ~c_cnt_y;
+                end
+            end
+            else if (in_valid) begin
+                first_line <= 1'b0;
+            end
+        end
+        wire [7:0] r_g_min = (r_odd < g_odd) ? r_odd : g_odd;
+        wire [7:0] r_g_b_min = (r_g_min < b_odd) ? r_g_min : b_odd;
+        wire [7:0] w_odd = r_g_b_min;
+        //wire [7:0] w_odd = 8'd0;
+        //rgb2y rgb2y_even (.r(r_odd), .g(g_odd), .b(b_odd), .y(w_odd));
+        assign y_odd[7:2] = (c_cnt_y == 1'b0) ? (r_odd) : (w_odd[7:2]);
+        assign y_even[7:2] = (c_cnt_y == 1'b0) ? (g_even) : (b_even);
+        assign y_odd[1:0] = y_odd[7:6];
+        assign y_even[1:0] = y_even[7:6];
+    end
+    endgenerate
 
     always @(posedge clk) begin
         out_valid <= in_valid;
